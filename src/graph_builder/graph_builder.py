9c77d8fd1ac8132b027abfa3bc937cf42d8c0d80
RichardHolzhofer <rchrdhlzhfr@gmail.com>
2025-12-08 14:52:51 +0100

    Added memory and memory handling to graph

--- src/graph_builder/graph_builder.py
+++ src/graph_builder/graph_builder.py
@@ -7,44 +7,34 @@ from src.config.config import Config
 from src.state.state import RAGState
 from src.node.node import RAGNodes
 
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph, END
+from langgraph.checkpoint.memory import MemorySaver
 
 class GraphBuilder:
     def __init__(self,retriever, llm):
-        self.nodes = RAGNodes(retriever=retriever, llm=llm)
+        self.nodes = RAGNodes(retriever=retriever,llm=llm)
         self.graph = None
+        self.memory = MemorySaver()
         
     def build_graph(self):
         graph_builder = StateGraph(RAGState)
         
+        graph_builder.add_node("rewriter", self.nodes.rewrite_query)
         graph_builder.add_node("retriever", self.nodes.retrieve_documents)
         #graph_builder.add_node("reranker", self.nodes.rerank_documents)
         graph_builder.add_node("answer_generator", self.nodes.generate_answer)
         
         
-        graph_builder.set_entry_point("retriever")
-
-        
+        graph_builder.set_entry_point("rewriter")
+      
         #graph_builder.add_edge("retriever", "reranker")
         
-        #graph_builder.add_edge("reranker", "answer_generator")
-        
+        graph_builder.add_edge("rewriter", "retriever")
         graph_builder.add_edge("retriever", "answer_generator")
 
-        
-        
         graph_builder.add_edge("answer_generator", END)
         
-        self.graph = graph_builder.compile()
+        self.graph = graph_builder.compile(checkpointer=self.memory)
         
         return self.graph
     
-    def run(self, question:str) -> dict:
-        if self.graph is None:
-            self.build_graph()
-        initialize_state = {
-            'question':question,
-            'retrieved_docs': [],
-            'answer':''
-            }
-        return self.graph.invoke(initialize_state)
--- src/state/state.py
+++ src/state/state.py
@@ -4,13 +4,17 @@ import sys
 from src.logger.logger import logging
 from src.exception.exception import RAGException
 
-from typing import List, TypedDict
+from typing import List, TypedDict, Annotated
 from pydantic import BaseModel
 from langchain_core.documents import Document
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 
 class RAGState(TypedDict):
     question: str
-    retrieved_docs: list[Document] = []
+    rewritten_query: str
+    retrieved_docs: List[Document] = []
+    chat_history: Annotated[List[BaseMessage], add_messages] = []
     answer: str = ""
     
     
